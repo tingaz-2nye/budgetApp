df8a663793833610e15a9c971af8e37ab8d5fc1a
tingaz-2nye <41738925+tingaz-2nye@users.noreply.github.com>
2023-02-10 21:09:36 +0200

    adding total budget and expenses overview element

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import ExpenseModal from './components/ExpenseModal';
 import SideBarNav from './components/SideBarNav';
 
 function App() {
+  const LOCAL_STORAGE_KEY = 'budgets';
   const [viewExpense, setViewExpense] = useState(false);
   const [addBudget, setAddBudget] = useState(false);
   const [addExpense, setAddExpense] = useState(false);
@@ -23,41 +24,47 @@ function App() {
   //   Amount: 0,
   //   budget_name: '',
   // });
-  const [budgets, setBudgets] = useState({
-    total: 2000,
-    month: 'Jan',
-    year: '2023',
-    budgets: [
-      {
-        id: uuidv4(),
-        title: 'Entertainment',
-        expenses: [],
-        budget_total: 5000,
-        expense_total: 0,
-      },
-      {
-        id: uuidv4(),
-        title: 'Food',
-        expenses: [],
-        budget_total: 2000,
-        expense_total: 0,
-      },
-      {
-        id: uuidv4(),
-        title: 'Transportation',
-        expenses: [],
-        budget_total: 2000,
-        expense_total: 0,
-      },
-      {
-        id: uuidv4(),
-        title: 'Rental Services',
-        expenses: [],
-        budget_total: 2000,
-        expense_total: 0,
-      },
-    ],
-  });
+  const [budgets, setBudgets] = useState(
+    JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || []
+
+    // {
+    // total: 2000,
+    // month: 'Jan',
+    // year: '2023',
+    // budgets:
+
+    // [
+    // {
+    //   id: uuidv4(),
+    //   title: 'Entertainment',
+    //   expenses: [],
+    //   budget_total: 5000,
+    //   expense_total: 0,
+    // },
+    // {
+    //   id: uuidv4(),
+    //   title: 'Food',
+    //   expenses: [],
+    //   budget_total: 2000,
+    //   expense_total: 0,
+    // },
+    // {
+    //   id: uuidv4(),
+    //   title: 'Transportation',
+    //   expenses: [],
+    //   budget_total: 2000,
+    //   expense_total: 0,
+    // },
+    // {
+    //   id: uuidv4(),
+    //   title: 'Rental Services',
+    //   expenses: [],
+    //   budget_total: 2000,
+    //   expense_total: 0,
+    // },
+    // ]
+    // }
+  );
 
   const handleOpenViewExpenseModal = () => setViewExpense(true);
   const handleCloseViewExpenseModal = () => setViewExpense(false);
@@ -69,9 +76,8 @@ function App() {
   const addSum = ({ budgetID, amount = 0, id = 0, isSum = true }) => {
     let sum = 0;
 
-    const expenses = budgets.budgets.filter(
-      (budget) => budget.id === budgetID
-    )[0]?.expenses;
+    const expenses = budgets.filter((budget) => budget.id === budgetID)[0]
+      ?.expenses;
 
     if (!isSum) {
       const newExpenses = expenses.filter((expense) => expense.id !== id);
@@ -88,20 +94,36 @@ function App() {
     return sum + parseFloat(amount);
   };
 
+  const overallBudgetTotal = () => {
+    let sum = 0;
+
+    for (const n of budgets) {
+      sum += parseFloat(n.budget_total);
+    }
+
+    return sum;
+  };
+  const overallExpenseTotal = () => {
+    let sum = 0;
+
+    for (const n of budgets) {
+      sum += parseFloat(n.expense_total);
+    }
+
+    return sum;
+  };
+
   const handleAddBudgetForm = (formValues) => {
     const { name, amount } = formValues;
     const obj = {
       id: uuidv4(),
       title: name,
       expenses: [],
-      budget_total: amount,
+      budget_total: parseFloat(amount),
       expense_total: 0,
     };
 
-    setBudgets({
-      ...budgets,
-      ['budgets']: [...budgets.budgets, obj],
-    });
+    setBudgets([...budgets, obj]);
     handleCloseAddBudgetModal();
   };
 
@@ -116,9 +138,8 @@ function App() {
 
     const sum = addSum({ budgetID, amount });
 
-    setBudgets({
-      ...budgets,
-      ['budgets']: budgets.budgets.map((budget) => {
+    setBudgets(
+      budgets.map((budget) => {
         return budget.id === budgetID
           ? {
               ...budget,
@@ -126,23 +147,19 @@ function App() {
               ['expense_total']: sum,
             }
           : budget;
-      }),
-    });
-
+      })
+    );
     handleCloseAddExpenseModal();
   };
 
   const handleDeleteExpense = (id, budgetID) => {
-    const expense = budgets.budgets.filter(
-      (budget) => budget.id === budgetID
-    )[0];
+    const expense = budgets.filter((budget) => budget.id === budgetID)[0];
     const expenses = expense.expenses.filter((exp) => exp.id !== id);
 
     const sum = addSum({ budgetID, isSum: false, id });
 
-    setBudgets({
-      ...budgets,
-      ['budgets']: budgets.budgets.map((budget) => {
+    setBudgets(
+      budgets.map((budget) => {
         return budget.id === budgetID
           ? {
               ...budget,
@@ -150,29 +167,28 @@ function App() {
               ['expense_total']: sum,
             }
           : budget;
-      }),
-    });
+      })
+    );
   };
 
   const handleDeleteBudget = (id) => {
-    const newBudgets = budgets.budgets.filter((budget) => budget.id !== id);
-    setBudgets({
-      ...budgets,
-      ['budgets']: newBudgets,
-    });
+    const newBudgets = budgets.filter((budget) => budget.id !== id);
+    setBudgets(newBudgets);
   };
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(budgets));
+  }, [budgets]);
 
   return (
     <>
-      <div className="bg-slate-800 w-screen h-screen overflow-x-hidden flex justify-start">
+      <div className="bg-slate-800 w-screen min-h-screen overflow-x-hidden flex justify-start">
         <SideBarNav handleOpenBudgetModal={handleOpenAddBudgetModal} />
 
-        <div className="w-full bg-slate-600 grid grid-cols-3 gap-4 pt-10 px-4">
-          {budgets.budgets.map((budget, index) => (
+        <div className="w-full h-auto pb-20 bg-slate-600 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-4 gap-x-3 pt-10 px-4 overflow-x-hidden">
+          {budgets.map((budget, index) => (
             <BudgetCard
-              key={budget.id}
+              key={index}
               budget={budget}
               handleOpenView={handleOpenViewExpenseModal}
               handleOpenAdd={handleOpenAddExpenseModal}
@@ -181,12 +197,15 @@ function App() {
             />
           ))}
         </div>
+
+        <div className="w-72 h-20 rounded-full bg-slate-900 fixed bottom-4 right-5 shadow-lg text-gray-400 justify-center flex items-center px-4 text-xl font-bold">
+          <div>Total :</div>
+          <div className="px-4 text-2xl text-gray-300">{`${overallExpenseTotal()} / ${overallBudgetTotal()}`}</div>
+        </div>
         <ExpenseModal
           open={viewExpense}
           handleCloseModal={handleCloseViewExpenseModal}
-          expenses={budgets.budgets.filter(
-            (budget) => budget.id === expenseCategory
-          )}
+          expenses={budgets.filter((budget) => budget.id === expenseCategory)}
           handleDeleteExpense={handleDeleteExpense}
         />
         <AddBudgetModal
@@ -197,10 +216,8 @@ function App() {
         <AddExpenseModal
           open={addExpense}
           handleSubmit={handleAddExpenseForm}
-          budgets={budgets.budgets}
-          expense={
-            budgets.budgets.filter((budget) => budget.id === expenseCategory)[0]
-          }
+          budgets={budgets}
+          expense={budgets.filter((budget) => budget.id === expenseCategory)[0]}
           handleCloseModal={handleCloseAddExpenseModal}
         />
       </div>
