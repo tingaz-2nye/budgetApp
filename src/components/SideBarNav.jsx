565b9d6e0232ad947e9b5e80009ff3413c1781cd
tingaz-2nye <41738925+tingaz-2nye@users.noreply.github.com>
2023-02-10 21:08:49 +0200

    changing to min-height-screen inorder for sidebar to fit horizontally when budget list grows

--- src/components/SideBarNav.jsx
+++ src/components/SideBarNav.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SideBarNav = ({ handleOpenBudgetModal }) => {
   return (
-    <div className="h-screen w-20 bg-slate-900 text-gray-300 py-4 flex flex-col gap-4 items-center">
+    <div className="min-h-screen w-20 bg-slate-900 text-gray-300 py-4 flex flex-col gap-4 items-center">
       <div className="rounded-full bg-white w-12 h-12"></div>
       <div className="mt-4">
         <button
