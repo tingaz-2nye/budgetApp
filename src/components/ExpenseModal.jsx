9a65e38b567515cc5a97cfc2d7c2cea47eafa71c
tingaz-2nye <tinga413@gmail.com>
2024-07-12 15:32:43 +0200

    feat: add expense date

--- src/components/ExpenseModal.jsx
+++ src/components/ExpenseModal.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import { CloseIcon, TrashIcon } from "../utils/icons";
 
 const ExpenseModal = ({
   open,
@@ -13,7 +14,7 @@ const ExpenseModal = ({
     <>
       <div
         className={`relative z-10 ${
-          !open ? 'hidden' : 'flex'
+          !open ? "hidden" : "flex"
         } transition ease-in-out duration-300 delay-75`}
         aria-labelledby="modal-title"
         role="dialog"
@@ -22,7 +23,7 @@ const ExpenseModal = ({
         <div className="fixed inset-0 bg-gray-500 bg-opacity-30 transition-opacity"></div>
         <div className="fixed inset-0 z-10 overflow-y-auto">
           <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-            <div className="relative transform overflow-hidden rounded-lg text-left shadow-xl transition-all sm:my-8 sm:w-72 sm:max-w-lg">
+            <div className="relative transform overflow-hidden rounded-lg text-left shadow-xl transition-all sm:my-8 sm:w-[800px] sm:max-w-lg">
               <div className="bg-slate-900  h-[700px] sm:h-[500px] overflow-x-hidden overflow-y-hidden">
                 <div className="sm:flex sm:items-start ">
                   <div className="mt-3 w-full text-center sm:mt-0 p-2 sm:text-left text-gray-300">
@@ -37,20 +38,7 @@ const ExpenseModal = ({
                         onClick={handleCloseModal}
                         className="cursor-pointer hover:text-red-600 hover:rounded-lg hover:w-6 hover:h-6"
                       >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={1.5}
-                          stroke="currentColor"
-                          className="w-6 h-6"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M6 18L18 6M6 6l12 12"
-                          />
-                        </svg>
+                        <CloseIcon />
                       </div>
                     </div>
                     <div className="flex flex-col px-2 py-2 h-[400px] overflow-y-scroll [&::-webkit-scrollbar]:hidden">
@@ -58,34 +46,30 @@ const ExpenseModal = ({
                       {expenses[0]?.expenses?.map((expense, index) => (
                         <div
                           key={index}
-                          className="py-2 px-2 mt-2 border border-rose-600 rounded-lg flex flex-row justify-between items-center"
+                          className="py-2 px-2 mt-2 border border-gray-400 rounded-lg flex-col justify-between items-center"
                         >
-                          <div className="text-sm text-gray-300 font-semibold">
-                            {expense.description}
-                          </div>
-                          <div className="flex items-center gap-4">
-                            <span>${expense.amount}</span>
-                            <button
-                              className="cursor-pointer hover:text-red-600 hover:rounded-lg hover:w-6 hover:h-6"
-                              onClick={() =>
-                                handleDelete(expense.id, expenses[0]?.id)
-                              }
-                            >
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth={1.5}
-                                stroke="currentColor"
-                                className="w-6 h-6"
+                          <div className="flex flex-row justify-between">
+                            <div className="text-xs text-red-600">
+                              {expense.date}
+                            </div>
+                            <div>
+                              <button
+                                className="cursor-pointer hover:text-red-600 hover:rounded-lg hover:w-4 hover:h-4"
+                                onClick={() =>
+                                  handleDelete(expense.id, expenses[0]?.id)
+                                }
                               >
-                                <path
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  d="M6 18L18 6M6 6l12 12"
-                                />
-                              </svg>
-                            </button>
+                                <TrashIcon height="w-4" width="w-4" />
+                              </button>
+                            </div>
+                          </div>
+                          <div className="flex flex-row justify-between items-center">
+                            <div className="text-sm text-gray-300 font-semibold">
+                              {expense.description}
+                            </div>
+                            <div className="flex items-center gap-4">
+                              <span>${expense.amount}</span>
+                            </div>
                           </div>
                         </div>
                       ))}
