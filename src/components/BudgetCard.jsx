0911b8afd28ad74fd241f1951b5e4b8dd57f3c57
tingaz-2nye <tinga413@gmail.com>
2024-07-12 13:00:46 +0200

    feat: add date to card and make it responsive

--- src/components/BudgetCard.jsx
+++ src/components/BudgetCard.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import { eyeIcon, plusIcon, trashIcon } from "../utils/icons";
 
 const colors = {
-  red: 'bg-red-600',
-  yellow: 'bg-yellow-600',
-  blue: 'bg-blue-600',
+  red: "bg-red-600",
+  yellow: "bg-yellow-600",
+  blue: "bg-blue-600",
 };
 
 const BudgetCard = ({
@@ -13,7 +14,16 @@ const BudgetCard = ({
   setExpenseCategory,
   handleDeleteBudget,
 }) => {
-  const { id, title, budget_total, expense_total, expenses } = budget;
+  const {
+    id,
+    title,
+    budget_total,
+    expense_total,
+    expenses,
+    fullDate,
+    month,
+    year,
+  } = budget;
   const [percentage, setPercentage] = useState(null);
 
   const [color, setColor] = useState(colors.blue);
@@ -37,10 +47,18 @@ const BudgetCard = ({
   }, [expense_total, budget_total, expenses]);
 
   return (
-    <div className="w-96 h-36 p-4 rounded-xl shadow-md bg-slate-900 text-gray-300 flex flex-col justify-between">
+    <div className="w-56 md:w-96 h-36 p-4 rounded-xl shadow-md bg-slate-900 text-gray-300 flex flex-col justify-between">
+      <div className="flex items-center">
+        <div className="text-xs text-gray-400">
+          {new Date(fullDate).toLocaleString("default", {
+            month: "long",
+            year: "numeric",
+          })}
+        </div>
+      </div>
       <div className="flex justify-between items-center">
         <div className="text-lg font-bold">{title}</div>
-        <div className="text-lg text-gray-400">
+        <div className="text-sm md:text-lg text-gray-400">
           {expense_total} / {budget_total}
         </div>
       </div>
@@ -56,24 +74,48 @@ const BudgetCard = ({
             handleOpenAdd();
             setExpenseCategory(id);
           }}
-          className="p-2 rounded-2xl uppercase text-sm bg-red-900 hover:bg-red-700"
+          className="p-2 hidden md:block rounded-2xl uppercase text-sm bg-red-900 hover:bg-red-700"
         >
           Add Expense
+        </button>
+        <button
+          onClick={() => {
+            handleOpenAdd();
+            setExpenseCategory(id);
+          }}
+          className="p-2 flex md:hidden rounded-2xl text-xs"
+        >
+          {plusIcon}
         </button>
         <button
           onClick={() => {
             handleOpenView();
             setExpenseCategory(id);
           }}
-          className="text-gray-400 rounded-2xl p-2 border border-red-900 hover:border-red-700 uppercase text-sm hover:text-gray-300"
+          className="text-gray-400 hidden md:block rounded-2xl p-2 border border-red-900 hover:border-red-700 uppercase text-sm hover:text-gray-300"
         >
           View Expenses
         </button>
         <button
-          className="text-gray-400 uppercase text-sm hover:text-gray-300"
+          onClick={() => {
+            handleOpenView();
+            setExpenseCategory(id);
+          }}
+          className="p-2 flex md:hidden text-xs rounded-2xl"
+        >
+          {eyeIcon}
+        </button>
+        <button
+          className="text-gray-400 hidden md:block uppercase text-sm hover:text-gray-300"
           onClick={() => handleDelete(id)}
         >
           Delete
+        </button>
+        <button
+          className="p-2 flex md:hidden text-xs rounded-2xl"
+          onClick={() => handleDelete(id)}
+        >
+          {trashIcon}
         </button>
       </div>
     </div>
