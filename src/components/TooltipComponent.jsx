56fb6860979a53ca05137cbffcd8746a8693d2e4
tingaz-2nye <tinga413@gmail.com>
2024-07-12 15:52:16 +0200

    refactor: change width of tooltip and change size of text

--- src/components/TooltipComponent.jsx
+++ src/components/TooltipComponent.jsx
@@ -4,7 +4,7 @@ const TooltipComponent = ({ children, text }) => {
   return (
     <div className="tooltip-container relative inline-block">
       <span className="tooltip-trigger">{children}</span>
-      <span className="tooltip rounded shadow-lg bg-rose-600 text-gray-300">
+      <span className="tooltip text-xs rounded shadow-lg bg-rose-600 text-gray-300">
         {text}
         <span className="tooltip-arrow"></span>
       </span>
